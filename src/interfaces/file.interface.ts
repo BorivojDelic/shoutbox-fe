e7dbed622cfa4559dc04b9c167c610e2c40e3548
Borivoj <borivojdelic@gmail.com>
2025-01-28 22:28:54 +0100

    Build basic structure

---
+++ src/interfaces/file.interface.ts
@@ -0,0 +1,5 @@
+import { EntityInterface } from "./entity.interface";
+
+export interface FileInterface extends EntityInterface{
+  id: number;
+}
---
+++ src/interfaces/message.interface.ts
@@ -0,0 +1,10 @@
+import { FileInterface } from "./file.interface";
+
+export interface MessageInterface {
+  id: number;
+  userIp: string;
+  userAgent: string;
+  message: string;
+  createdAt: string;
+  files: FileInterface[];
+}
